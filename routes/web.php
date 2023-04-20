4e7541852abbd3c9d6d3aed2d868df5f640d9b7a
seregandreev <seregandreev6@gmail.com>
2023-04-20 14:59:18 +0300

    Auth пользователей

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,9 @@ use App\Http\Controllers\BbsController;
 // });
 
 Route::get('/', [BbsController::class, 'index'])->name('index');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::get('/{bb}', [BbsController::class, 'detail'])->name('detail');
